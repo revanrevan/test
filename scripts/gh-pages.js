4de67e4a8b9cb702cd63ef875f6f03e7596c2bd9
revanrevan <revainblue21@gmail.com>
2021-01-17 20:56:03 +0700

    mengupdate untuk yang pertama

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -1,13 +1,13 @@
 const ghpages = require('gh-pages');
 
 ghpages.publish(
-    '__sapper__/export/<your-app-name>',
+    '__sapper__/export/test',
     {
         branch: 'master',
-        repo: 'https://github.com/<your-github-username>/<your-repo-name>.git',
+        repo: 'https://github.com/revanrevan/test.git',
         user: {
-            name: '<your-github-username>',
-            email: '<your-github-email>'
+            name: 'revanrevan',
+            email: 'revainblue21@gmail.com'
         }
     },
     () => {
